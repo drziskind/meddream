2a7ec521aa235773f6510215d1781e8bd0f66d19
Dave <drziskind@gmail.com>
2023-07-28 19:22:51 -0400

    removing apache pid file on startup

--- alpine/start.sh
+++ alpine/start.sh
@@ -1,4 +1,5 @@
 #!/bin/ash
 sh -c /startCore.sh >> /logs/startJavaCore.stdout.log 2>> /logs/startJavaCore.stderr.log &
+rm /run/apache2/httpd.pid
 httpd -D FOREGROUND >> /logs/apache.stdout.log 2>> /logs/apache.stderr.log
 ash
